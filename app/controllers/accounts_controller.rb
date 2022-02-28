341e1165df17ab367d8e5e38fb0082fc5af537ef
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-02-28 12:09:10 +0100

    add method create

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -10,4 +10,21 @@ class AccountsController < ApplicationController
   def new
     @account = Account.new
   end
+
+  def create
+    @account = Account.new(account_params)
+    @account.user = current_user
+    if @account.save
+      redirect_to accounts_path
+    else
+      puts @account.user
+      render :new
+    end
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:name, :bank, :fund)
+  end
 end
