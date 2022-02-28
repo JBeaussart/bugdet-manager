d385b0d421597d26827d98a45be24569620cb241
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-02-28 18:07:40 +0100

    add method new, create and associate view

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,31 @@
 class TransactionsController < ApplicationController
+  before_action :find_account, only: [:new]
+
   def index
     @transactions = Transaction.where(account_id: current_user.accounts.ids)
+  end
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    @transaction.account = current_user.accounts.ids
+    if @transaction.save
+      redirect_to account_transactions_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:date, :amount, :tag, :comment)
+  end
+
+  def find_account
+    @account = Account.find(params[:account_id])
   end
 end
