200403240150f4a32265785e40055558024b1b7f
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-03-01 16:34:56 +0100

    home#index in the root and add title

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'accounts#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :accounts do
     resources :transactions, except: [:show]
