8e504e394947fecb25d602e57461ac87303ab13e
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-03-01 15:36:28 +0100

    add method to update account fund when update transaction amount

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,10 +3,16 @@ class Transaction < ApplicationRecord
   validates :date, presence: true
   validates :amount, presence: true
   after_create :update_account_fund_create
+  before_update :update_account_fund_update
   after_destroy :update_account_fund_delete
 
   def update_account_fund_create
     account.fund += amount
+    account.save
+  end
+
+  def update_account_fund_update
+    account.fund += amount - amount_was
     account.save
   end
 
