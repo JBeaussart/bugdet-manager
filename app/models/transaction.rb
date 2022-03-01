978c1a0e4ec9aa6a5afa48e87369826f9b402d99
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-03-01 14:21:12 +0100

    create method to update fund value when transaction is delete

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,10 +2,16 @@ class Transaction < ApplicationRecord
   belongs_to :account
   validates :date, presence: true
   validates :amount, presence: true
-  after_create :update_account_fund
+  after_create :update_account_fund_create
+  after_destroy :update_account_fund_delete
 
-  def update_account_fund
+  def update_account_fund_create
     account.fund += amount
+    account.save
+  end
+
+  def update_account_fund_delete
+    account.fund -= amount
     account.save
   end
 end
