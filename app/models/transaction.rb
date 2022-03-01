d3b4778dde20ede21b4091d270ce74d2a7f23626
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-03-01 11:53:27 +0100

    increase account fund when create a new transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,10 @@ class Transaction < ApplicationRecord
   belongs_to :account
   validates :date, presence: true
   validates :amount, presence: true
+  after_create :update_account_fund
+
+  def update_account_fund
+    account.fund += amount
+    account.save
+  end
 end
