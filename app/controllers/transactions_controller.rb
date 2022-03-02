3a82a130be273b9f25ac15e8c93589f6f9071f6a
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-03-02 13:03:45 +0100

    remove useless navbar link, order transactions by date

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,7 +3,7 @@ class TransactionsController < ApplicationController
   before_action :find_transaction, only: %i[edit update destroy]
 
   def index
-    @transactions = Transaction.where(account_id: find_account)
+    @transactions = Transaction.where(account_id: find_account).order('date asc')
   end
 
   def new
