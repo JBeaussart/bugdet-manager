8400216e3fb252d62e5f3e5f58c5f0e93b1ccfef
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-03-09 12:34:49 +0100

    add homepage from account controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -33,6 +33,10 @@ class AccountsController < ApplicationController
     redirect_to accounts_path
   end
 
+  def home
+    @accounts = Account.where(user: current_user)
+  end
+
   private
 
   def find_account
