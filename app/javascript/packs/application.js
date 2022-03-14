55c81f8b8bca4846da4b7f953de037be6c1e55aa
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-03-14 12:22:51 +0100

    add table for total year bi tags

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,7 @@ import "controllers"
 import "bootstrap"
 
 const button = document.querySelector('.amount_month_tags_details')
-const amounts = document.querySelector('.amount_month_tags_details_amount')
+const amounts = document.querySelector('.amount_month_tags')
 
 button.addEventListener('click', () => {
     if (amounts.style.display === 'none') {
