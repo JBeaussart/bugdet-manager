76872b683517cde5db1e86ae7b915d9e57bf69cf
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-03-14 11:48:19 +0100

    add button to show amount detail tags

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,16 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+const button = document.querySelector('.amount_month_tags_details')
+const amounts = document.querySelector('.amount_month_tags_details_amount')
+
+button.addEventListener('click', () => {
+    if (amounts.style.display === 'none') {
+      amounts.style.display = 'block';
+      button.innerHTML = 'Voir moins'
+    } else {
+      amounts.style.display = 'none'
+      button.innerHTML = 'Voir le detail'
+    }
+})
