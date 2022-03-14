c7d8b1bdf2cc6fc5cebc6d939ad2d3d09b5feaab
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-03-14 15:19:05 +0100

    add chartjs

--- Gemfile
+++ Gemfile
@@ -28,6 +28,9 @@ gem 'redis', '~> 4.0'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+gem 'groupdate'
+gem 'chartkick'
+
 gem 'devise'
 
 gem 'autoprefixer-rails', '10.2.5'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,11 +19,14 @@ const button = document.querySelector('.amount_month_tags_details')
 const amounts = document.querySelector('.amount_month_tags')
 
 button.addEventListener('click', () => {
-    if (amounts.style.display === 'none') {
-      amounts.style.display = 'block';
-      button.innerHTML = 'Voir moins'
-    } else {
-      amounts.style.display = 'none'
-      button.innerHTML = 'Voir le detail'
-    }
+  if (amounts.style.display === 'none') {
+    amounts.style.display = 'block';
+    button.innerHTML = 'Voir moins'
+  } else {
+    amounts.style.display = 'none'
+    button.innerHTML = 'Voir le detail'
+  }
 })
+
+require("chartkick")
+require("chart.js")
