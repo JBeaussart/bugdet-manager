325afd46fdbe2dbe63c0811461c6b2d6690453bc
jeremy beaussart <jeremy.beaussart@gmail.com>
2022-03-21 17:59:33 +0100

    don't need to add first and last name to sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   has_many :accounts, dependent: :destroy
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
